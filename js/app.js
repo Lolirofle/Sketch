55ef14c35152afe4d0dab4ca101ee10f32647c7e
Lolirofle <lolipopple@hotmail.com>
2016-09-30 00:22:23 +0200

    Almost working in an element

--- js/app.js
+++ js/app.js
@@ -1,40 +1,93 @@
 function Sketcher(elem){
-	var sketcher_this = this;
+	var sketcher = this;
 
-	this.canvas = document.getElementById("canvas");
-	this.canvas.onmousedown = function(event){
-		sketcher_this.painting = true;
+	//Elements
+	this.canvas = {};
+	this.canvas.elem    = document.getElementById("canvas");
+	this.canvas.context = this.canvas.elem.getContext("2d");
 
-		sketcher_this.currentStroke = new Stroke();
-		sketcher_this.currentStroke.tool = sketcher_this.tool;
+	this.buffer = {};
+	this.buffer.elem    = document.getElementById("buffer");
+	this.buffer.context = this.buffer.elem.getContext("2d");
 
-		sketcher_this.ctx.fillStyle = sketcher_this.penStyle;
-		sketcher_this.lastX = event.pageX - this.offsetLeft;
-		sketcher_this.lastY = event.pageY - this.offsetTop;
+	this.sketch = {};
+	this.sketch.elem    = document.getElementById("sketch");
+	this.sketch.context = this.sketch.elem.getContext("2d");
+
+	//States
+	this.isStroking = false;
+	this.w = 1200;
+	this.h = 1600;
+	this.strokes = [];
+	this.currentStroke;
+	this.redoStrokesStack = [];
+	this.lastX = 0;
+	this.lastY = 0;
+
+	//Settings
+	this.lineThickness = 5; //px
+	this.thicknessEase = 3;
+	this.maxThickness = 10; //px
+	this.penStyle    = "rgba(0,0,0,0.5)";       //CanvasContext.fillStyle
+	this.fadeStyle   = "rgba(0,0,0,0)";         //CanvasContext.fillStyle
+	this.backStyle   = "rgba(240,240,240,1)";   //CanvasContext.fillStyle
+	this.eraserStyle = "rgba(240,240,240,0.3)"; //CanvasContext.fillStyle
+	this.fadeBack    = "rgba(240,240,240,0)";
+	this.undoLimit = 16; //UInt
+	this.tool = Tool.Pen; //Tool
+
+	//Intialization
+	this.canvas.elem.onkeydown = function(event){
+		switch(event.keyCode){
+			case 90://z
+				if(event.ctrlKey){
+					if(event.shiftKey){
+						sketcher.redoStroke();
+					}else{
+						sketcher.undoStroke();
+					}
+				}
+				break;
+			case 89://y
+				if(event.ctrlKey){
+					sketcher.redoStroke();
+				}
+				break;
+		}
+	};
+	this.canvas.elem.onmousedown = function(event){
+		sketcher.isStroking = true;
+
+		sketcher.currentStroke = new Stroke();
+		sketcher.currentStroke.tool = sketcher.tool;
+
+		sketcher.canvas.context.fillStyle = sketcher.penStyle;
+		sketcher.lastX = event.clientX;
+		sketcher.lastY = event.clientY;
 	};
 
-	this.canvas.onmouseup = function(event){
-		sketcher_this.painting = false;
+	this.canvas.elem.onmouseup = function(event){
+		sketcher.isStroking = false;
 
-		sketcher_this.sketchctx.drawImage(canvas,0,0);
-		sketcher_this.addStroke();
+		sketcher.sketch.context.drawImage(canvas,0,0);
+		sketcher.addStroke();
 
-		sketcher_this.ctx.clearRect(0,0,canvas.width,canvas.height);
+		sketcher.canvas.context.clearRect(0,0,canvas.width,canvas.height);
 	}
 
-	this.canvas.onmousemove = function(event){
-		if(sketcher_this.painting){
-			mouseX = event.pageX - this.offsetLeft;
-			mouseY = event.pageY - this.offsetTop;
+	this.canvas.elem.onmousemove = function(event){
+		if(sketcher.isStroking){
+			var mouseX = event.clientX;
+			var mouseY = event.clientY;
 
 			// find all points between
 			var x1 = mouseX,
-				x2 = sketcher_this.lastX,
+				x2 = sketcher.lastX,
 				y1 = mouseY,
-				y2 = sketcher_this.lastY;
+				y2 = sketcher.lastY;
 
-			switch(sketcher_this.tool){
-				default:
+			switch(sketcher.tool){
+				case Tool.Pen:
 					var steep = (Math.abs(y2 - y1) > Math.abs(x2 - x1));
 					if(steep){
 						var x = x1;
@@ -66,31 +119,31 @@ function Sketcher(elem){
 						yStep = 1;
 					}
 
-					var targetLineThickness = sketcher_this.maxThickness - Math.sqrt((x2 - x1) *(x2-x1) + (y2 - y1) * (y2-y1))/(10);
-					if(sketcher_this.lineThickness > targetLineThickness + sketcher_this.thicknessEase){
-						sketcher_this.lineThickness -= sketcher_this.thicknessEase;
+					var targetLineThickness = sketcher.maxThickness - Math.sqrt((x2 - x1) *(x2-x1) + (y2 - y1) * (y2-y1))/(10);
+					if(sketcher.lineThickness > targetLineThickness + sketcher.thicknessEase){
+						sketcher.lineThickness -= sketcher.thicknessEase;
 					}
-					else if(sketcher_this.lineThickness < targetLineThickness - sketcher_this.thicknessEase){
-						sketcher_this.lineThickness += sketcher_this.thicknessEase;
+					else if(sketcher.lineThickness < targetLineThickness - sketcher.thicknessEase){
+						sketcher.lineThickness += sketcher.thicknessEase;
 					}
 					else{
-						sketcher_this.lineThickness = targetLineThickness;
+						sketcher.lineThickness = targetLineThickness;
 					}
-					if(sketcher_this.lineThickness < 1){
-						sketcher_this.lineThickness = 1;
+					if(sketcher.lineThickness < 1){
+						sketcher.lineThickness = 1;
 					}
 
 					for (var x = x1; x < x2; x++){
 						if(steep){
-							sketcher_this.currentStroke.x.push(y);
-							sketcher_this.currentStroke.y.push(x);
-							sketcher_this.currentStroke.thickness.push(sketcher_this.lineThickness);
-							sketcher_this.drawStyle(sketcher_this.ctx,y,x,sketcher_this.lineThickness,sketcher_this.tool);
+							sketcher.currentStroke.x.push(y);
+							sketcher.currentStroke.y.push(x);
+							sketcher.currentStroke.thickness.push(sketcher.lineThickness);
+							sketcher.drawWithStyle(sketcher.canvas.context,y,x,sketcher.lineThickness,sketcher.tool);
 						} else{
-							sketcher_this.currentStroke.x.push(x);
-							sketcher_this.currentStroke.y.push(y);
-							sketcher_this.currentStroke.thickness.push(sketcher_this.lineThickness);
-							sketcher_this.drawStyle(sketcher_this.ctx,x,y,sketcher_this.lineThickness,sketcher_this.tool);
+							sketcher.currentStroke.x.push(x);
+							sketcher.currentStroke.y.push(y);
+							sketcher.currentStroke.thickness.push(sketcher.lineThickness);
+							sketcher.drawWithStyle(sketcher.canvas.context,x,y,sketcher.lineThickness,sketcher.tool);
 						}
 
 						error += de;
@@ -101,70 +154,39 @@ function Sketcher(elem){
 					}
 
 
-					sketcher_this.lastX = mouseX;
-					sketcher_this.lastY = mouseY;
+					sketcher.lastX = mouseX;
+					sketcher.lastY = mouseY;
 				break;
 
-				case Tool.Pen:
-					x = event.pageX - sketcher_this.offsetLeft;
-					y = event.pageY - sketcher_this.offsetTop;
-
+				case Tool.Eraser:
 					// the distance the mouse has moved since last mousemove event
-					var dis = Math.sqrt(Math.pow(sketcher_this.lastX-x,2)+Math.pow(sketcher_this.lastY-y,2));
+					var dis = Math.sqrt(Math.pow(sketcher.lastX-x,2)+Math.pow(sketcher.lastY-y,2));
 
 					// for each pixel distance,draw a circle on the line connecting the two points
 					// to get a continous line.
 					for (i=0;i<dis;i+=1){
 						var s = i/dis;
-						sketcher_this.currentStroke.x.push(sketcher_this.lastX*s + x*(1-s));
-						sketcher_this.currentStroke.y.push(sketcher_this.lastY*s + y*(1-s));
-						sketcher_this.currentStroke.thickness.push(w);
-						sketcher_this.drawStyle(ctx,sketcher_this.lastX*s + x*(1-s),sketcher_this.lastY*s + y*(1-s),w,tool);
+						sketcher.currentStroke.x.push(sketcher.lastX*s + mouseX*(1-s));
+						sketcher.currentStroke.y.push(sketcher.lastY*s + mouseY*(1-s));
+						sketcher.currentStroke.thickness.push(w);
+						sketcher.drawWithStyle(
+							context,
+							sketcher.lastX*s + mouseX*(1-s),
+							sketcher.lastY*s + mouseY*(1-s),
+							w,
+							sketcher.tool
+						);
 					}
-					sketcher_this.lastX = x;
-					sketcher_this.lastY = y;
+					sketcher.lastX = mouseX;
+					sketcher.lastY = mouseY;
 				break;
 
 			}
 
 		}
 	}
-	this.ctx = canvas.getContext("2d");
-	this.painting = false;
-	this.lastX = 0;
-	this.lastY = 0;
-	this.lineThickness = 1;
-	this.thicknessEase = 0.5;
-
-	this.buffer = document.getElementById("buffer");
-	this.bufferctx = this.buffer.getContext("2d");
-
-	this.w = 1200;
-	this.h = 1600;
-	this.sketch = document.getElementById("sketch");
-	this.sketchctx = this.sketch.getContext("2d");
-
-	this.maxThickness = 3;
-
-	this.penStyle = "rgba(0,0,0,0.5)";
-	this.fadeStyle = "rgba(0,0,0,0)";
-	this.backStyle = "rgba(240,240,240,1)";
-	this.eraserStyle = "rgba(240,240,240,0.3)";
-	this.fadeBack = "rgba(240,240,240,0)";
-
-	this.strokes = [];
-	this.currentStroke;
-
-	this.undoLimit = 16;
-
-	this.redoStack = [];
-
-	this.tool = 0;
-	// 0 = pen
-	// 1 = eraser
-
 	this.resizeCanvas(this.w,this.h);
-	this.drawBackground(this.sketchctx);
+	this.drawBackground(this.sketch.context);
 
 	//window.addEventListener('resize',this.resizeCanvas(),false);
 }
@@ -173,162 +195,155 @@ function Stroke(){
 	this.x = [];
 	this.y = [];
 	this.thickness = [];
-	this.tool = 0;
+	this.tool = Tool.Pen;
 }
 
 Sketcher.prototype.newCanvas = function(){
-	this.bufferctx.clearRect(0,0,w,h);
-	this.bufferctx.drawImage(this.sketch,0,0);
-	this.drawBackground(this.sketchctx);
+	this.buffer.context.clearRect(0,0,this.w,this.h);
+	this.buffer.context.drawImage(this.sketch.elem,0,0);
+	this.drawBackground(this.sketch.context);
 
 	this.strokes = [];
-	this.redoStack = [];
-
-	this.strokes.push(new Stroke());
+	this.redoStrokesStack = [];
 }
 
 
 Sketcher.prototype.resizeCanvas = function(w,h){
-	this.canvas.width = w;
-	this.canvas.height = h;
-	this.sketch.width = w;
-	this.sketch.height = h;
+	this.canvas.elem.width  = w;
+	this.buffer.elem.width  = w;
+	this.sketch.elem.width  = w;
 
-	this.buffer.width = w;
-	this.buffer.height = h;
-
-	$(".surface").css("margin-left",function(){ return -w/2; });
+	this.canvas.elem.height = h;
+	this.sketch.elem.height = h;
+	this.buffer.elem.height = h;
 }
 
-Sketcher.prototype.fillCircle = function(ctx,x,y,radius){
-	ctx.beginPath();
-	ctx.arc(x,y,radius,0,2 * Math.PI,false);
-	ctx.fillStyle = this.penStyle;
-	ctx.fill();
+Sketcher.prototype.fillCircle = function(context,x,y,radius){
+	context.beginPath();
+	context.arc(x,y,radius,0,2 * Math.PI,false);
+	context.fillStyle = this.penStyle;
+	context.fill();
 }
 
-Sketcher.prototype.fillCircleEraser = function(ctx,x,y,radius){
-	ctx.beginPath();
-	ctx.arc(x,y,radius,0,2 * Math.PI,false);
-	ctx.fillStyle = this.eraserStyle;
-	ctx.fill();
+Sketcher.prototype.fillCircleEraser = function(context,x,y,radius){
+	context.beginPath();
+	context.arc(x,y,radius,0,2 * Math.PI,false);
+	context.fillStyle = this.eraserStyle;
+	context.fill();
 }
 
-Sketcher.prototype.fillEraser = function(ctx,x,y,radius){
-	var gradient = ctx.createRadialGradient(x,y,0,x,y,radius);
+Sketcher.prototype.fillEraser = function(context,x,y,radius){
+	var gradient = context.createRadialGradient(x,y,0,x,y,radius);
 	gradient.addColorStop(0,this.eraserStyle);
 	gradient.addColorStop(1,this.fadeBack);
 
-	ctx.beginPath();
-	ctx.arc(x,y,w,0,2 * Math.PI);
-	ctx.fillStyle = gradient;
-	ctx.fill();
-	ctx.closePath();
+	context.beginPath();
+	context.arc(x,y,w,0,2 * Math.PI);
+	context.fillStyle = gradient;
+	context.fill();
+	context.closePath();
 }
 
-Sketcher.prototype.drawBackground = function(ctx){
-	ctx.clearRect(0,0,this.w,this.h);
-	ctx.fillStyle = this.backStyle;
-	ctx.fillRect(0,0,this.w,this.h);
+Sketcher.prototype.drawBackground = function(context){
+	context.clearRect(0,0,this.w,this.h);
+	context.fillStyle = this.backStyle;
+	context.fillRect(0,0,this.w,this.h);
 }
 
-Sketcher.prototype.drawStyle = function(ctx,x,y,thickness,tool){
-
+Sketcher.prototype.drawWithStyle = function(context,x,y,thickness,tool){
 	switch(tool){
-		default:
-			// THIN INK //
-			ctx.fillRect(x,y,thickness,thickness);
-			this.fillCircle(ctx,x+thickness/4,y+thickness/4,thickness/2);
+		case Tool.Pen:
+			context.fillRect(x,y,thickness,thickness);
+			this.fillCircle(context,x+thickness/4,y+thickness/4,thickness/2);
 		break;
 
-		case 1:
-			// ERASER //
-			var markerWidth = 15;
-			this.fillCircleEraser(ctx,x+markerWidth/2,y+markerWidth/2,markerWidth);
+		case Tool.Eraser:
+			var markerWidth = 15;//TODO
+			this.fillCircleEraser(context,x+markerWidth/2,y+markerWidth/2,markerWidth);
 		break;
 	}
 
 	// THIN PEN //
-	//ctx.fillRect(x,y,thickness,thickness);
+	//context.fillRect(x,y,thickness,thickness);
 
 	// THIN INK //
-	//ctx.fillRect(x,y,thickness,thickness);
-	//this.fillCircle(ctx,x+thickness/4,y+thickness/4,thickness/2);
+	//context.fillRect(x,y,thickness,thickness);
+	//this.fillCircle(context,x+thickness/4,y+thickness/4,thickness/2);
 
 	// INK PEN
-	//ctx.fillRect(x,y,thickness,thickness);
-	//this.fillCircle(ctx,x,y,thickness);
+	//context.fillRect(x,y,thickness,thickness);
+	//this.fillCircle(context,x,y,thickness);
 
 	// MARKER
 
 }
 
-Sketcher.prototype.drawStroke = function(ctx,stroke){
+Sketcher.prototype.drawStroke = function(context,stroke){
 	for (var i = 0; i < stroke.x.length; i += 1){
-		this.drawStyle(ctx,stroke.x[i],stroke.y[i],stroke.thickness[i],stroke.tool);
+		this.drawWithStyle(context,stroke.x[i],stroke.y[i],stroke.thickness[i],stroke.tool);
 	}
 }
 
-Sketcher.prototype.drawAllStrokes = function(ctx){
-	ctx.clearRect(0,0,w,h);
-	this.drawBackground(ctx);
-	ctx.fillStyle = this.penStyle;
+Sketcher.prototype.drawAllStrokes = function(context){
+	context.clearRect(0,0,w,h);
+	this.drawBackground(context);
+	context.fillStyle = this.penStyle;
 	for (var i = 0; i < this.strokes.length; i += 1){
 		var s = this.strokes[i];
-		this.drawStroke(ctx,s);
+		this.drawStroke(context,s);
 	}
 }
-Sketcher.prototype.drawAllStrokesNoClear = function(ctx){
-	ctx.fillStyle = this.penStyle;
+Sketcher.prototype.drawAllStrokesNoClear = function(context){
+	context.fillStyle = this.penStyle;
 	for (var i = 0; i < this.strokes.length; i += 1){
 		var s = this.strokes[i];
-		this.drawStroke(ctx,s);
+		this.drawStroke(context,s);
 	}
 }
 Sketcher.prototype.undoStroke = function(){
 	if(this.strokes.length>0){
-		this.drawBackground(this.sketchctx);
-		this.redoStack.push(this.strokes.pop());
-		this.sketchctx.drawImage(this.buffer,0,0);
-		this.drawAllStrokesNoClear(this.sketchctx);
-		console.log("undo");
+		this.drawBackground(this.sketch.context);
+		this.redoStrokesStack.push(this.strokes.pop());
+		this.sketch.context.drawImage(this.buffer.elem,0,0);
+		this.drawAllStrokesNoClear(this.sketch.context);
 	}
 }
 
 Sketcher.prototype.addStroke = function(){
 	if(this.currentStroke.x.length > 2){
-		this.redoStack = [];
+		this.redoStrokesStack = [];
 		this.strokes.push(this.currentStroke);
+		this.currentStroke = null;
 
 		if(this.strokes.length > this.undoLimit){
 			this.strokes.reverse();
 			var oldStroke = this.strokes.pop();
 			this.strokes.reverse();
 
-			this.drawStroke(this.bufferctx,oldStroke);
+			this.drawStroke(this.buffer.context,oldStroke);
 		}
 		else{
 			/*if(newlyCreated){
-				this.bufferctx.clearRect(0,0,w,h);
+				this.buffer.context.clearRect(0,0,w,h);
 				newlyCreated = false;
 			}*/
 
 		}
-		//this.drawAllStrokes(this.sketchctx);
+		//this.drawAllStrokes(this.sketch.context);
 	}
 
 }
 Sketcher.prototype.redoStroke = function(){
-	if(this.redoStack.length>0){
-		this.drawBackground(this.sketchctx);
-		this.strokes.push(this.redoStack.pop());
-		this.sketchctx.drawImage(this.buffer,0,0);
-		this.drawAllStrokesNoClear(this.sketchctx);
+	if(this.redoStrokesStack.length>0){
+		this.drawBackground(this.sketch.context);
+		this.strokes.push(this.redoStrokesStack.pop());
+		this.sketch.context.drawImage(this.buffer.elem,0,0);
+		this.drawAllStrokesNoClear(this.sketch.context);
 	}
 
 }
 
 var Tool = {
-	Pen   : 1,
-	Eraser: 2,
+	Pen   : 0,
+	Eraser: 1,
 };
